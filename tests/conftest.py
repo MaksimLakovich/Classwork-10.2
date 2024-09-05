ee9813fce0d2c3df138ced5cf7958096d73dc0bc
Maksim Lakovich <maks.lakovich@gmail.com>
2024-09-05 18:28:25 +0300

    'Добавлены все необходимые тесты и покрытие равно 100%'

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,10 @@ import pytest
 
 
 @pytest.fixture
-def prices():
+def prices_fixture():
     return [100, 230]
 
 
 @pytest.fixture
-def taxed_prices():
+def taxed_prices_fixture():
     return [150, 345]
--- tests/test_calculate_taxes.py
+++ tests/test_calculate_taxes.py
@@ -1,13 +1,22 @@
 import pytest
 
 from src.main import calculate_taxes
+from tests.conftest import prices_fixture, taxed_prices_fixture
 
 
+@pytest.mark.parametrize("prices, taxed_prices",
+                         [
+                             ([100, 200, 2000], [150, 300, 3000]),
+                         ])
 def test_calculate_taxes_positive_case(prices, taxed_prices):
     assert calculate_taxes(prices, 50) == taxed_prices
 
 
-def test_calculate_taxes_errors(prices, taxed_prices):
+def test_calculate_taxes_errors(prices_fixture, taxed_prices_fixture):
     with pytest.raises(ValueError) as exc_info:
-        calculate_taxes(prices, -10)
+        calculate_taxes(prices_fixture, -10)
     assert str(exc_info.value) == "Неверный налоговый процент"
+
+    with pytest.raises(ValueError) as exception_info:
+        calculate_taxes([0, -1], 50)
+    assert str(exception_info.value) == "Неверная цена"
