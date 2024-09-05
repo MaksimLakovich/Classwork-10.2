5f1a5906bec7df0a7adea6068fbfa8019ced7b28
Maksim Lakovich <maks.lakovich@gmail.com>
2024-09-05 20:06:15 +0300

    Завершил реализацию функции my_calculate_tax по методологии TDD

--- src/my_function.py
+++ src/my_function.py
@@ -4,4 +4,7 @@ def my_calculate_tax(price: float, tax_rate: float) -> float:
     if price < 0:
         raise ValueError("Неверная цена")
 
+    if tax_rate >= 100 or tax_rate < 0:
+        raise ValueError("Неверный налоговый процент")
+
     return price + (price * tax_rate / 100)
