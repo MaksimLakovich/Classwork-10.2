1335b7e22c5d234076731db8fdbe42415b583af0
Maksim Lakovich <maks.lakovich@gmail.com>
2024-09-06 13:50:38 +0300

    'Используя методологию TDD доработал функцию и добавил новые 4 проверки/условия и тесты к этим новым условиям'

--- src/my_function.py
+++ src/my_function.py
@@ -1,5 +1,15 @@
-def my_calculate_tax(price: float, tax_rate: float) -> float:
-    """Функция вычисляет стоимость товара с учетом налога"""
+def my_calculate_tax(
+        price: float,
+        tax_rate: float,
+        discount: float = 0
+) -> float:
+    """Функция вычисляет стоимость товара с учетом налога и скидки.
+    Скидка берется от цены с учетом налога.
+    """
+
+    for arg in [price, tax_rate, discount]:
+        if not isinstance(arg, (int | float)):
+            raise TypeError("Некорректный тип данных")
 
     if price < 0:
         raise ValueError("Неверная цена")
@@ -7,4 +17,7 @@ def my_calculate_tax(price: float, tax_rate: float) -> float:
     if tax_rate >= 100 or tax_rate < 0:
         raise ValueError("Неверный налоговый процент")
 
-    return price + (price * tax_rate / 100)
+    price_with_tax = price + (price * tax_rate / 100)
+    price_with_discount = price_with_tax - price_with_tax / 100 * discount
+
+    return round(price_with_discount, 2)
--- tests/test_my_function.py
+++ tests/test_my_function.py
@@ -5,12 +5,24 @@ from src.my_function import my_calculate_tax
 
 @pytest.mark.parametrize("price, tax_rate, result",
                          [
-                             (1, 10, 1.1),
-                             (20.20, 11.1, 22.4422000000),
+                             (20.20, 11.1, 22.44),
                              (0, 10, 0),
                          ])
 def test_calculate_tax(price, tax_rate, result):
     assert my_calculate_tax(price, tax_rate) == result
+
+
+@pytest.mark.parametrize("price, tax_rate, discount, result",
+                         [
+                             (1, 10, 50, 0.55),
+                             (100, 20, 25, 90),
+                         ])
+def test_with_discount(price, tax_rate, discount, result):
+    assert my_calculate_tax(price, tax_rate, discount) == result
+
+
+def test_without_discount():
+    assert my_calculate_tax(100, 20) == 120
 
 
 def test_price_errors():
@@ -26,6 +38,12 @@ def test_tax_rate_errors():
 
 
 def test_big_tax_rate_errors():
-    with pytest.raises(ValueError) as info_exc:
+    with pytest.raises(ValueError) as info_exception:
         my_calculate_tax(100, 100)
-    assert str(info_exc.value) == "Неверный налоговый процент"
+    assert str(info_exception.value) == "Неверный налоговый процент"
+
+
+def test_wrong_type():
+    with pytest.raises(TypeError) as info_exception:
+        my_calculate_tax("500", "25", "15")
+    assert str(info_exception.value) == "Некорректный тип данных"
